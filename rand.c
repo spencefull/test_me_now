a931693937015895effe6a137074163ef56b1c51
spencersamuel7 <spencersamuel7@gmail.com>
2019-02-25 13:35:43 -0600

    file

---
+++ rand.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void init_rand(void)
+{
+	srand(time(NULL));
+	//comment here
+}
+
+// TODO: use a better random generator
+int random_int(int max)
+{
+	return rand() % max;
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <number>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	int max = atoi(argv[1]);
+
+	init_rand();
+	int result = random_int(max);
+	printf("%d\n", result);
+
+	return EXIT_SUCCESS;
+}
